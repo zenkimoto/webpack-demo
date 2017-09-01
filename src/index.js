c04e2622a06a351ae78642df0988f2f42310db2b
alexyip123 <alexyip123@gmail.com>
2017-09-01 11:11:33 -0700

    Added css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import _ from 'lodash';
 import $ from 'jquery';
+import './style.css';
 
 function component() {
-    return $("<div/>").html(_.join(['Hello', 'webpack', 'Alex'], ' '));
+    return $("<div/>").html(_.join(['Hello', 'webpack', 'Alex'], ' ')).addClass('red');
   }
   
-$('body').append(component())
+$('body').append(component());
 
   
   
