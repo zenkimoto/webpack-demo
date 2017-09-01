826a58fa8a0cc6e01779b3fbc5e6b837bad8a0e8
alexyip123 <alexyip123@gmail.com>
2017-09-01 15:25:42 -0700

    Added babel loader

--- src/index.js
+++ src/index.js
@@ -3,9 +3,21 @@ import $ from 'jquery';
 
 function component() {
     return $("<div/>").html(_.join(['Hello', 'webpack', 'Alex'], ' '));
-  }
+}
   
 $('body').append(component())
 
   
-  
+let p = new Promise(function(resolve, reject) {  
+    resolve(1);
+ });
+
+p.then(function(result) {
+    console.log("result: ", result);
+});
+
+let funcName = (params) => params + 2
+
+let result = funcName(2);
+
+console.log("result", result);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        loaders: [
+            {
+                test: /.js$/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    devtool: 'source-map'
 };
