9b19def4c86d2235b737b9e05b3ed8fa539b1572
alexyip123 <alexyip123@gmail.com>
2017-09-01 16:39:34 -0700

    Cleaned up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,54 +2,35 @@ const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = function(env) {
+    let config = {
+        entry: './src/index.js',
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, 'dist')
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['env']
+                        }
+                    }
+                }
+            ]
+        },
+        devtool: 'source-map'
+    };
+
     if (env && env.prod) {
-        return {
-            entry: './src/index.js',
-            output: {
-                filename: 'bundle.js',
-                path: path.resolve(__dirname, 'dist')
-            },
-            module: {
-                rules: [
-                    {
-                        test: /\.js$/,
-                        use: {
-                            loader: 'babel-loader',
-                            options: {
-                                presets: ['env']
-                            }
-                        }
-                    }
-                ]
-            },
-            plugins: [
-                new UglifyJSPlugin()
-            ],
-            devtool: 'source-map'
-        };
-    } else {
-        return {
-            entry: './src/index.js',
-            output: {
-                filename: 'bundle.js',
-                path: path.resolve(__dirname, 'dist')
-            },
-            module: {
-                rules: [
-                    {
-                        test: /\.js$/,
-                        use: {
-                            loader: 'babel-loader',
-                            options: {
-                                presets: ['env']
-                            }
-                        }
-                    }
-                ]
-            },
-            devtool: 'source-map'
-        };
+        config.plugins = [
+            new UglifyJSPlugin()
+        ]
     }
+
+    return config;
 }
 
 
