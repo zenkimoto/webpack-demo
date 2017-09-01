d18e243396306ffdd0986976a58b375da140069f
alexyip123 <alexyip123@gmail.com>
2017-09-01 16:23:44 -0700

    Added prod and dev build configurations

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,55 @@
 const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env']
+module.exports = function(env) {
+    if (env && env.prod) {
+        return {
+            entry: './src/index.js',
+            output: {
+                filename: 'bundle.js',
+                path: path.resolve(__dirname, 'dist')
+            },
+            module: {
+                rules: [
+                    {
+                        test: /\.js$/,
+                        use: {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: ['env']
+                            }
+                        }
                     }
-                }
-            }
-        ]
-    },
-    plugins: [
-        //new UglifyJSPlugin()
-    ],
-    devtool: 'source-map'
-};
+                ]
+            },
+            plugins: [
+                new UglifyJSPlugin()
+            ],
+            devtool: 'source-map'
+        };
+    } else {
+        return {
+            entry: './src/index.js',
+            output: {
+                filename: 'bundle.js',
+                path: path.resolve(__dirname, 'dist')
+            },
+            module: {
+                rules: [
+                    {
+                        test: /\.js$/,
+                        use: {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: ['env']
+                            }
+                        }
+                    }
+                ]
+            },
+            devtool: 'source-map'
+        };
+    }
+}
+
+
