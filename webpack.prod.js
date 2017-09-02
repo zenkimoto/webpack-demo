23b426466bc445bcbba7f152452d5f6c4b250caa
alexyip123 <alexyip123@gmail.com>
2017-09-02 11:45:31 -0700

    Added purify css webpack plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,19 @@
 const merge = require('webpack-merge');
+const path = require('path');
 const common = require('./webpack.common.js');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const PurifyCSSPlugin = require('purifycss-webpack');
+const glob = require('glob');
 
 module.exports = merge(common, {
     plugins: [
-        new UglifyJSPlugin()
+        new UglifyJSPlugin(),
+        new PurifyCSSPlugin({
+            // Give paths to parse for rules. These should be absolute!
+            paths: glob.sync(path.join(__dirname, 'index.html')),
+            purifyOptions: {
+                minify: true
+            }
+        }),
     ]
 });
