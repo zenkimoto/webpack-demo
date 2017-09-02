15dd97bfa80aa61c46d19e9fba7f36931511b08b
alexyip123 <alexyip123@gmail.com>
2017-09-02 10:09:19 -0700

    Updated webpack configuration for dev and prod

---
+++ webpack.common.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
+    }
+}
+
+
