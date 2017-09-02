2ed6daf14ca9220573e892d8c07f525ef3ae2181
alexyip123 <alexyip123@gmail.com>
2017-09-02 10:33:39 -0700

    Updated webpack config to generate two different bundles

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import _ from 'lodash';
 import $ from 'jquery';
+import printMe from './print.js';
 import './style.css';
 
 function component() {
-    return $("<div/>").html(_.join(['Hello', 'webpack', 'Alex'], ' ')).addClass('red');
+    return $("<div/>").html(_.join(['Hello', 'webpack', 'Alex'], ' ')).addClass('red').append($('<button/>').click(printMe).html('Click Me'));
   }
   
 $('body').append(component());
